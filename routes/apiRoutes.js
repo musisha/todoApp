c5bef4a843ba22ac80d6e644ef8f205d43449186
mark musisha <markmusisha@hotmail.com>
2019-06-23 15:19:32 +0200

    initial commit of the todo app

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const apiControllers = require('../controllers/apiControllers');
+
+//get all the tasks available
+
+
+//add new task
+router.post('/add/:name/:subtasks/:isCompleted', apiControllers.postAddTask);
+
+//get a specific task
+router.get('/search/:name', apiControllers.searchTask);
+
+//update task
+router.put('/update-task', apiControllers.updateTask);
+
+router.put('/add-sub-tasks/names/name/isCompleted', apiControllers.AddSubTask);
+
+
+
+
+module.exports = router;
